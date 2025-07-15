8555b5c111bf53dc9fa6627c066d600ac0f0d494
Danila Maksimenko <109770253+Dan-maksi@users.noreply.github.com>
2025-07-15 11:07:00 -0700

    Add files via upload

---
+++ SQL Scripts/Create external table hive.sql
@@ -0,0 +1,139 @@
+create schema hive.demo;
+
+CREATE TABLE hive.demo.currencyexchange (
+    date VARCHAR,
+    fromcurrency VARCHAR,
+    tocurrency VARCHAR,
+    exchange VARCHAR
+)
+WITH (
+    format = 'CSV',
+    external_location = 's3a://demo/test/currencyexchange/',
+    skip_header_line_count = 1,
+    csv_quote = '"',
+    csv_escape = '"'
+);
+CREATE TABLE hive.demo.customer (
+    customerkey VARCHAR,
+    geoareakey VARCHAR,
+    startdt VARCHAR,
+    enddt VARCHAR,
+    continent VARCHAR,
+    gender VARCHAR,
+    title VARCHAR,
+    givenname VARCHAR,
+    middleinitial VARCHAR,
+    surname VARCHAR,
+    streetaddress VARCHAR,
+    city VARCHAR,
+    state VARCHAR,
+    statefull VARCHAR,
+    zipcode VARCHAR,
+    country VARCHAR,
+    countryfull VARCHAR,
+    birthday VARCHAR,
+    age VARCHAR,
+    occupation VARCHAR,
+    company VARCHAR,
+    vehicle VARCHAR,
+    latitude VARCHAR,
+    longitude VARCHAR
+)
+WITH (
+    format = 'CSV',
+    external_location = 's3a://demo/test/customer/',
+    skip_header_line_count = 1,
+    csv_quote = '"',
+    csv_escape = '"'
+);
+CREATE TABLE hive.demo.date (
+    date VARCHAR,
+    datekey VARCHAR,
+    year VARCHAR,
+    yearquarter VARCHAR,
+    yearquarternumber VARCHAR,
+    quarter VARCHAR,
+    yearmonth VARCHAR,
+    yearmonthshort VARCHAR,
+    yearmonthnumber VARCHAR,
+    month VARCHAR,
+    monthshort VARCHAR,
+    monthnumber VARCHAR,
+    dayofweek VARCHAR,
+    dayofweekshort VARCHAR,
+    dayofweeknumber VARCHAR,
+    workingday VARCHAR,
+    workingdaynumber VARCHAR
+)
+WITH (
+    format = 'CSV',
+    external_location = 's3a://demo/test/date/',
+    skip_header_line_count = 1,
+    csv_quote = '"',
+    csv_escape = '"'
+);
+CREATE TABLE hive.demo.product (
+    productkey VARCHAR,
+    productcode VARCHAR,
+    productname VARCHAR,
+    manufacturer VARCHAR,
+    brand VARCHAR,
+    color VARCHAR,
+    weightunit VARCHAR,
+    weight VARCHAR,
+    cost VARCHAR,
+    price VARCHAR,
+    categorykey VARCHAR,
+    categoryname VARCHAR,
+    subcategorykey VARCHAR,
+    subcategoryname VARCHAR
+)
+WITH (
+    format = 'CSV',
+    external_location = 's3a://demo/test/product/',
+    skip_header_line_count = 1,
+    csv_quote = '"',
+    csv_escape = '"'
+);
+CREATE TABLE hive.demo.sales (
+    orderkey VARCHAR,
+    linenumber VARCHAR,
+    orderdate VARCHAR,
+    deliverydate VARCHAR,
+    customerkey VARCHAR,
+    storekey VARCHAR,
+    productkey VARCHAR,
+    quantity VARCHAR,
+    unitprice VARCHAR,
+    netprice VARCHAR,
+    unitcost VARCHAR,
+    currencycode VARCHAR,
+    exchangerate VARCHAR
+)
+WITH (
+    format = 'CSV',
+    external_location = 's3a://demo/test/sales/',
+    skip_header_line_count = 1,
+    csv_quote = '"',
+    csv_escape = '"'
+);
+CREATE TABLE hive.demo.store (
+    storekey VARCHAR,
+    storecode VARCHAR,
+    geoareakey VARCHAR,
+    countrycode VARCHAR,
+    countryname VARCHAR,
+    state VARCHAR,
+    opendate VARCHAR,
+    closedate VARCHAR,
+    description VARCHAR,
+    squaremeters VARCHAR,
+    status VARCHAR
+)
+WITH (
+    format = 'CSV',
+    external_location = 's3a://demo/test/store/',
+    skip_header_line_count = 1,
+    csv_quote = '"',
+    csv_escape = '"'
+);
---
+++ SQL Scripts/Hive_sql.sql
@@ -0,0 +1,149 @@
+
+
+CREATE TABLE iceberg.demo.stg_date AS
+SELECT
+    CAST(date AS DATE) AS date,
+    CAST(datekey AS BIGINT) AS datekey,
+    CAST(year AS INTEGER) AS year,
+    yearquarter,
+    CAST(yearquarternumber AS BIGINT) AS yearquarternumber,
+    quarter,
+    yearmonth,
+    yearmonthshort,
+    CAST(yearmonthnumber AS BIGINT) AS yearmonthnumber,
+    month,
+    monthshort,
+    CAST(monthnumber AS INTEGER) AS monthnumber,
+    dayofweek,
+    dayofweekshort,
+    CAST(dayofweeknumber AS INTEGER) AS dayofweeknumber,
+    CAST(workingday AS BOOLEAN) AS workingday,
+    CAST(workingdaynumber AS INTEGER) AS workingdaynumber
+FROM hive.demo.date;
+
+CREATE TABLE iceberg.demo.stg_currencyexchange AS
+SELECT
+    CAST(date AS DATE) AS date,
+    fromcurrency,
+    tocurrency,
+    CAST(exchange AS DOUBLE) AS exchange
+FROM hive.demo.currencyexchange;
+
+CREATE TABLE iceberg.demo.stg_customer AS
+SELECT
+    CAST(customerkey AS BIGINT) AS customerkey,
+    CAST(geoareakey AS BIGINT) AS geoareakey,
+    CAST(startdt AS DATE) AS startdt,
+    CAST(enddt AS DATE) AS enddt,
+    continent,
+    gender,
+    title,
+    givenname,
+    middleinitial,
+    surname,
+    streetaddress,
+    city,
+    state,
+    statefull,
+    zipcode,
+    country,
+    countryfull,
+    CAST(birthday AS DATE) AS birthday,
+    CAST(age AS INTEGER) AS age,
+    occupation,
+    company,
+    vehicle,
+    CAST(latitude AS DOUBLE) AS latitude,
+    CAST(longitude AS DOUBLE) AS longitude
+FROM hive.demo.customer;
+
+
+CREATE TABLE iceberg.demo.stg_product AS
+SELECT
+    CASE 
+        WHEN TRIM(productkey) = '' THEN NULL
+        ELSE CAST(productkey AS BIGINT)
+    END AS productkey,
+    productcode,
+    productname,
+    manufacturer,
+    brand,
+    color,
+    weightunit,
+    CASE 
+        WHEN TRIM(weight) = '' THEN NULL
+        ELSE CAST(weight AS DOUBLE)
+    END AS weight,
+    CASE 
+        WHEN TRIM(cost) = '' THEN NULL
+        ELSE CAST(cost AS DOUBLE)
+    END AS cost,
+    CASE 
+        WHEN TRIM(price) = '' THEN NULL
+        ELSE CAST(price AS DOUBLE)
+    END AS price,
+    CASE 
+        WHEN TRIM(categorykey) = '' THEN NULL
+        ELSE CAST(categorykey AS BIGINT)
+    END AS categorykey,
+    categoryname,
+    CASE 
+        WHEN TRIM(subcategorykey) = '' THEN NULL
+        ELSE CAST(subcategorykey AS BIGINT)
+    END AS subcategorykey,
+    subcategoryname
+FROM hive.demo.product;
+
+CREATE TABLE iceberg.demo.stg_sales AS
+SELECT
+    CAST(orderkey AS BIGINT) AS orderkey,
+    CAST(linenumber AS INTEGER) AS linenumber,
+    CAST(orderdate AS DATE) AS orderdate,
+    CAST(deliverydate AS DATE) AS deliverydate,
+    CAST(customerkey AS BIGINT) AS customerkey,
+    CAST(storekey AS BIGINT) AS storekey,
+    CAST(productkey AS BIGINT) AS productkey,
+    CAST(quantity AS INTEGER) AS quantity,
+    CAST(unitprice AS DOUBLE) AS unitprice,
+    CAST(netprice AS DOUBLE) AS netprice,
+    CAST(unitcost AS DOUBLE) AS unitcost,
+    currencycode,
+    CAST(exchangerate AS DOUBLE) AS exchangerate
+FROM hive.demo.sales;
+
+CREATE TABLE iceberg.demo.stg_store AS
+SELECT
+    CAST(storekey AS BIGINT) AS storekey,
+    storecode,
+    CAST(geoareakey AS BIGINT) AS geoareakey,
+    countrycode,
+    countryname,
+    state,
+    -- handle empty strings for opendate
+    CASE 
+        WHEN TRIM(opendate) = '' THEN NULL
+        ELSE CAST(opendate AS DATE)
+    END AS opendate,
+    -- handle empty strings for closedate
+    CASE 
+        WHEN TRIM(closedate) = '' THEN NULL
+        ELSE CAST(closedate AS DATE)
+    END AS closedate,
+    description,
+    -- handle empty strings for squaremeters
+    CASE 
+        WHEN TRIM(squaremeters) = '' THEN NULL
+        ELSE CAST(squaremeters AS INTEGER)
+    END AS squaremeters,
+    status
+FROM hive.demo.store;
+
+SELECT
+    c.stg_customerkey,
+    c.stg_customername,
+    CAST(c.stg_age AS INTEGER) AS stg_age
+FROM iceberg.demo.stg_customer c
+WHERE CAST(c.stg_age AS INTEGER) > 50
+LIMIT 100;
+
+
